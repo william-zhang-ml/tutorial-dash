f5043a9ba018ca4cd9f7810b52db7401701c4d5f
William Zhang <william.zhang.ml@gmail.com>
2025-04-10 10:18:00 -0400

    add dropdown menu for image selection (only first few)

--- coco-app/app.py
+++ coco-app/app.py
@@ -1,3 +1,4 @@
+
 """
 An application to teach me the following skills that are good to know.
 - display an image
@@ -5,14 +6,17 @@ An application to teach me the following skills that are good to know.
 - switch the image from a dropdown menu
 - switch the image randomly from a button press
 """
+import base64
+from io import BytesIO
 import logging
-from dash import Dash, html
+from dash import Dash, html, dcc, callback, Input, Output, no_update
 import hydra
 from omegaconf import DictConfig
 from torchvision.datasets import CocoDetection
 
 
 app = Dash()
+dataset = None
 
 
 @hydra.main(version_base=None, config_path='.', config_name='default')
@@ -22,14 +26,46 @@ def launch_app(cfg: DictConfig) -> None:
     Args:
         cfg (DictConfig): app configuration
     """
+    global dataset
     dataset = CocoDetection(
         root=cfg.image_path,
         annFile=cfg.annot_path
     )
     img, _ = dataset[0]  # img is PIL
 
-    app.layout = html.Img(src=img)
+    # dashboard structure
+    app.layout = html.Div([
+        dcc.Dropdown(
+            list(range(cfg.num_dropdown)),
+            0,
+            id='img-selection'
+        ),
+        html.Img(src=img, id='img')
+    ])
+
     app.run(debug=True)
+
+
+# Dropdown menu callback
+@callback(
+    Output('img', 'src'),
+    Input('img-selection', 'value')
+)
+def update_graph(idx: int) -> None:
+    """Extract population data for a specific country.
+
+    Args:
+        value (str): country of interest
+
+    Returns:
+        px.line: population versus year data
+    """
+    if idx is None:
+        return no_update
+
+    global dataset
+    img, _ = dataset[int(idx)]
+    return img
 
 
 if __name__ == '__main__':
