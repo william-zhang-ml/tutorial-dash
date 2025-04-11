53a851d42ddceb1dbc0096b08165aaf500faa5fe
William Zhang <william.zhang.ml@gmail.com>
2025-04-11 05:23:35 -0400

    some commenting and refactoring

--- 01-datatable-app/app.py
+++ 01-datatable-app/app.py
@@ -1,23 +1,37 @@
 """
 Example from https://dash.plotly.com/datatable.
 """
-from dash import Dash, Input, Output, callback, dash_table
+from dash import Dash, Input, Output, dash_table
 import pandas as pd
 import dash_bootstrap_components as dbc
 
+
+# Underlying app data in tabular form
 df = pd.read_csv('https://git.io/Juf1t')
 
+# The app itself
 app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
+# Define the front-end layout
 app.layout = dbc.Container([
     dbc.Label('Click a cell in the table:'),
-    dash_table.DataTable(df.to_dict('records'),[{"name": i, "id": i} for i in df.columns], id='tbl'),
+    dash_table.DataTable(
+        df.to_dict('records'),
+        [{"name": i, "id": i} for i in df.columns],
+        id='tbl'
+    ),
     dbc.Alert(id='tbl_out'),
 ])
 
-@callback(Output('tbl_out', 'children'), Input('tbl', 'active_cell'))
+
+# Changes the alert message
+@app.callback(
+    Output('tbl_out', 'children'),
+    Input('tbl', 'active_cell')
+)
 def update_graphs(active_cell):
     return str(active_cell) if active_cell else "Click the table"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
