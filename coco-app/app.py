84ad5c88256c4fa715305edf8720cc29770302a7
William Zhang <william.zhang.ml@gmail.com>
2025-04-13 00:11:44 -0400

    add image display attached to randomizer button

--- coco-app/app.py
+++ coco-app/app.py
@@ -2,19 +2,24 @@
 Application for browsing object detection datasets.
 """
 import random
+from typing import Dict, List, Tuple
 from dash import (
     Dash,
     dcc,
-    Input, Output, State
+    html,
+    Input, Output, State, no_update
 )
 import dash_bootstrap_components as dbc
 import hydra
 from omegaconf import DictConfig
+from PIL.Image import Image
+from PIL.ImageDraw import Draw
 from torchvision.datasets import CocoDetection
 
 
 IMG_STORE_ID = 'img-store'
 OVERLAY_STORE_ID = 'overlay-store'
+IMAGE_ID = 'image'
 BUTTON_ID = 'randomizer'
 
 
@@ -36,6 +41,28 @@ def launch_app(cfg: DictConfig = None) -> None:
         [
             dbc.Col(
                 [
+                    html.Div(
+                        html.Img(
+                            src=None,
+                            id=IMAGE_ID,
+                            style={
+                                'max-height': '100%',
+                                'max-width': '100%',
+                                'height': 'auto',
+                                'width': 'auto',
+                                'object-fit': 'contain'
+                            }
+                        ),
+                        style={
+                            'height': '648px',
+                            'width': '648px',
+                            'padding': '4px',
+                            'border': '1px solid black',
+                            'display': 'flex',
+                            'align-items': 'center',
+                            'justify-content': 'center'
+                        }
+                    ),
                     dbc.Button(
                         'Random',
                         color='primary',
@@ -85,7 +112,7 @@ def launch_app(cfg: DictConfig = None) -> None:
     ]
 )
 def select_new_image(_, img_store) -> dict:
-    """Determine if app needs to acquire a different image.
+    """Determine if app needs to get a different image.
 
     Args:
         _ (_type_): _description_
@@ -93,11 +120,42 @@ def select_new_image(_, img_store) -> dict:
     Returns:
         dict: _description_
     """
-    new_store = img_store.copy()
     idx = random.randint(0, img_store['num_images'] - 1)
+    if idx == img_store['idx']:
+        return no_update
+    new_store = img_store.copy()
     new_store['idx'] = idx
-    print(new_store)
     return new_store
+
+
+@app.callback(
+    Output(IMAGE_ID, 'src'),
+    Input(IMG_STORE_ID, 'data')
+)
+def display_new_image(img_store) -> Image:
+    """_summary_
+
+    Args:
+        img_store (_type_): _description_
+
+    Returns:
+        Image: _description_
+    """
+    img, _ = get_image(img_store['idx'])
+    return img
+
+
+def get_image(idx: int) -> Tuple[Image, List[Dict]]:
+    """Get the idx-th image.
+
+    Args:
+        idx (int): dataset index
+
+    Returns:
+        Tuple[Image, List[Dict]]: image, instance annotations
+    """
+    global dataset
+    return dataset[idx]
 
 
 if __name__ == '__main__':
